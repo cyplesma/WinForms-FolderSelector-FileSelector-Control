2ee117774898bf3839a887541c60b221c8caef37
Mike <cyplesma@aol.com>
2026-08-11 20:07:49 -0400

    Add files via upload

---
+++ ucFileSelector.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TVShowSeasonFileStructureSetup
+{
+    public partial class ucFileSelector : UserControl
+    {
+        public event EventHandler FileNameChanged;
+
+        private string sFileType = "";
+        private bool bSelectFolder = false;
+        private string _lastCommittedFileName = "";
+
+        public ucFileSelector()
+        {
+            InitializeComponent();
+
+            txtFullFileName.Leave += txtFullFileName_Leave;
+        }
+
+        public string FileType
+        {
+            get { return sFileType; }
+            set { sFileType = value ?? ""; }
+        }
+
+        public bool SelectFolder
+        {
+            get { return bSelectFolder; }
+            set { bSelectFolder = value; }
+        }
+
+        public override string Text
+        {
+            get { return txtFullFileName.Text; }
+            set
+            {
+                txtFullFileName.Text = value ?? "";
+                CommitFileNameChange();
+            }
+        }
+
+        public string FileName
+        {
+            get { return txtFullFileName.Text; }
+            set
+            {
+                txtFullFileName.Text = value ?? "";
+                CommitFileNameChange();
+            }
+        }
+
+        private void txtFullFileName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                CommitFileNameChange();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void btnOpenFileExplorer_Click(object sender, EventArgs e)
+        {
+            if (SelectFolder)
+            {
+                using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+                {
+                    fbd.Description = "Select Folder";
+
+                    if (Directory.Exists(txtFullFileName.Text))
+                        fbd.SelectedPath = txtFullFileName.Text;
+
+                    if (fbd.ShowDialog() == DialogResult.OK)
+                    {
+                        txtFullFileName.Text = fbd.SelectedPath;
+                        CommitFileNameChange();
+                    }
+                }
+            }
+            else
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
+                    if (string.IsNullOrEmpty(sFileType))
+                        ofd.Filter = "All Files (*.*)|*.*";
+                    else
+                        ofd.Filter = sFileType;
+
+                    if (File.Exists(txtFullFileName.Text))
+                        ofd.FileName = txtFullFileName.Text;
+
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        txtFullFileName.Text = ofd.FileName;
+                        CommitFileNameChange();
+                    }
+                }
+            }
+        }
+
+        private void txtFullFileName_Leave(object sender, EventArgs e)
+        {
+            CommitFileNameChange();
+        }
+
+        private void CommitFileNameChange()
+        {
+            string currentValue = txtFullFileName.Text ?? "";
+
+            if (string.Equals(currentValue, _lastCommittedFileName, StringComparison.Ordinal))
+                return;
+
+            _lastCommittedFileName = currentValue;
+            OnFileNameChanged(EventArgs.Empty);
+        }
+
+        protected virtual void OnFileNameChanged(EventArgs e)
+        {
+            FileNameChanged?.Invoke(this, e);
+        }
+
+        private void ucFileSelector_Resize(object sender, EventArgs e)
+        {
+            btnOpenFileExplorer.Left = this.Width - (btnOpenFileExplorer.Width + 10);
+            txtFullFileName.Width = btnOpenFileExplorer.Left - 10;
+        }
+
+    }
+}
---
+++ ucFileSelector.designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TVShowSeasonFileStructureSetup
+
+{
+    partial class ucFileSelector
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFileExplorer = new System.Windows.Forms.Button();
+            this.txtFullFileName = new System.Windows.Forms.TextBox();
+            this.ofdFileExplorer = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnOpenFileExplorer
+            // 
+            this.btnOpenFileExplorer.Location = new System.Drawing.Point(582, 0);
+            this.btnOpenFileExplorer.Margin = new System.Windows.Forms.Padding(6);
+            this.btnOpenFileExplorer.Name = "btnOpenFileExplorer";
+            this.btnOpenFileExplorer.Size = new System.Drawing.Size(54, 37);
+            this.btnOpenFileExplorer.TabIndex = 0;
+            this.btnOpenFileExplorer.Text = "...";
+            this.btnOpenFileExplorer.UseVisualStyleBackColor = true;
+            this.btnOpenFileExplorer.Click += new System.EventHandler(this.btnOpenFileExplorer_Click);
+            // 
+            // txtFullFileName
+            // 
+            this.txtFullFileName.Location = new System.Drawing.Point(6, 0);
+            this.txtFullFileName.Margin = new System.Windows.Forms.Padding(6);
+            this.txtFullFileName.Name = "txtFullFileName";
+            this.txtFullFileName.Size = new System.Drawing.Size(564, 32);
+            this.txtFullFileName.TabIndex = 1;
+            this.txtFullFileName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtFullFileName_KeyDown);
+            // 
+            // ofdFileExplorer
+            // 
+            this.ofdFileExplorer.CheckFileExists = false;
+            this.ofdFileExplorer.FileName = "Folder Selection";
+            this.ofdFileExplorer.ValidateNames = false;
+            // 
+            // ucFileSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtFullFileName);
+            this.Controls.Add(this.btnOpenFileExplorer);
+            this.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "ucFileSelector";
+            this.Size = new System.Drawing.Size(652, 42);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFileExplorer;
+        private System.Windows.Forms.TextBox txtFullFileName;
+        private System.Windows.Forms.OpenFileDialog ofdFileExplorer;
+    }
+}
